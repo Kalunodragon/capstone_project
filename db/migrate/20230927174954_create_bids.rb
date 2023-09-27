79aaeba403d5634047bc6b24f7cae51faa581382
Andrew <andrewonulak@gmail.com>
2023-09-27 11:55:35 -0600

    creating table data for Bids

--- db/migrate/20230927174954_create_bids.rb
+++ db/migrate/20230927174954_create_bids.rb
@@ -1,6 +1,9 @@
 class CreateBids < ActiveRecord::Migration[7.0]
   def change
     create_table :bids do |t|
+      t.integer :choice_number
+      t.integer :employee_id
+      t.integer :schedule_id
 
       t.timestamps
     end
