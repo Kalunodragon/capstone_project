1bcdca2878e05dec356f3750200509d996d2e4ae
Andrew <andrewonulak@gmail.com>
2023-09-27 14:44:46 -0600

    creating custom method shift_info to get all info for a specific schedules shifts

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,21 @@
 class Schedule < ApplicationRecord
     has_many :bids
     has_many :shifts
+
+    def shift_info
+        week = []
+        info = []
+        week << self.sunday_shift
+        week << self.monday_shift
+        week << self.tuesday_shift
+        week << self.wednesday_shift
+        week << self.thursday_shift
+        week << self.friday_shift
+        week << self.saturday_shift
+
+        week.each do |shift_id|
+            info << Shift.find_by(id: shift_id)
+        end
+        info
+    end
 end
