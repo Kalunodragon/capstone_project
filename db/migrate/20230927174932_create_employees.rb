044f903bf99d80d8c643e9aad4264f27be27bda4
Andrew <andrewonulak@gmail.com>
2023-09-27 11:54:18 -0600

    creating table data for Employees

--- db/migrate/20230927174932_create_employees.rb
+++ db/migrate/20230927174932_create_employees.rb
@@ -1,7 +1,16 @@
 class CreateEmployees < ActiveRecord::Migration[7.0]
   def change
     create_table :employees do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.string :department
+      t.integer :phone_number
+      t.string :email
+      t.string :station
+      t.date :seniority_date
+      t.date :date_of_birth
+      t.boolean :admin
+      
       t.timestamps
     end
   end
