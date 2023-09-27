e9f763a749f583d48105ff9ac307aa7d82e2a8c5
Andrew <andrewonulak@gmail.com>
2023-09-27 14:33:44 -0600

    changing seed data so that Date information is added correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require 'date'
+
+p Date.new(1992,5,2)
+
 p "--- Starting Seed Generator ---"
 p " "
 p "Generating Employees"
-    Employee.create(first_name:"Andrew", last_name:"Onulak", department:"Ramp", phone_number:7325759043, email:"test@test.com", station:"DEN", seniority_date:2016-05-19, date_of_birth:1992-05-02, admin:false)
+    Employee.create(first_name:"Andrew", last_name:"Onulak", department:"Ramp", phone_number:7325759043, email:"test@test.com", station:"DEN", seniority_date: Date.new(2016,5,19), date_of_birth: Date.new(1992,5,2), admin:false)
 p "Finished generating Employees"
 p " "
 p "Generating Shifts"
@@ -24,10 +28,10 @@ p "Generating Shifts"
 p "Finished generating Shifts"
 p " "
 p "Generating Schedules"
-    Schedule.create(bid_open:2023-9-1, bid_close:2023-9-8, start_date: 2023-10-8, end_date: 2023-11-11, sunday_shift:1, monday_shift:1, tuesday_shift:2, wednesday_shift:2, thursday_shift:2, friday_shift:2, saturday_shift:2, number_available:3)
-    Schedule.create(bid_open:2023-9-1, bid_close:2023-9-8, start_date: 2023-10-8, end_date: 2023-11-11, sunday_shift:3, monday_shift:3, tuesday_shift:1, wednesday_shift:1, thursday_shift:3, friday_shift:3, saturday_shift:3, number_available:2)
-    Schedule.create(bid_open:2023-9-1, bid_close:2023-9-8, start_date: 2023-10-8, end_date: 2023-11-11, sunday_shift:4, monday_shift:4, tuesday_shift:4, wednesday_shift:4, thursday_shift:1, friday_shift:1, saturday_shift:4, number_available:1)
-    Schedule.create(bid_open:2023-9-1, bid_close:2023-9-8, start_date: 2023-10-8, end_date: 2023-11-11, sunday_shift:7, monday_shift:7, tuesday_shift:7, wednesday_shift:1, thursday_shift:1, friday_shift:7, saturday_shift:7, number_available:5)
+    Schedule.create(bid_open:Date.new(2023,1,9), bid_close:Date.new(2023,8,9), start_date: Date.new(2023,8,10), end_date: Date.new(2023,11,11), sunday_shift:1, monday_shift:1, tuesday_shift:2, wednesday_shift:2, thursday_shift:2, friday_shift:2, saturday_shift:2, number_available:3)
+    Schedule.create(bid_open:Date.new(2023,1,9), bid_close:Date.new(2023,8,9), start_date: Date.new(2023,8,10), end_date: Date.new(2023,11,11), sunday_shift:3, monday_shift:3, tuesday_shift:1, wednesday_shift:1, thursday_shift:3, friday_shift:3, saturday_shift:3, number_available:2)
+    Schedule.create(bid_open:Date.new(2023,1,9), bid_close:Date.new(2023,8,9), start_date: Date.new(2023,8,10), end_date: Date.new(2023,11,11), sunday_shift:4, monday_shift:4, tuesday_shift:4, wednesday_shift:4, thursday_shift:1, friday_shift:1, saturday_shift:4, number_available:1)
+    Schedule.create(bid_open:Date.new(2023,1,9), bid_close:Date.new(2023,8,9), start_date: Date.new(2023,8,10), end_date: Date.new(2023,11,11), sunday_shift:7, monday_shift:7, tuesday_shift:7, wednesday_shift:1, thursday_shift:1, friday_shift:7, saturday_shift:7, number_available:5)
 p "Finished generating Schedules"
 p " "
 p "Generating Bids"
