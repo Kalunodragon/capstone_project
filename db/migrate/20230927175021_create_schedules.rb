e3c038b9b20b9e028020455d2220f1c28c6a6556
Andrew <andrewonulak@gmail.com>
2023-09-27 11:59:24 -0600

    creating table data for Schedules

--- db/migrate/20230927175021_create_schedules.rb
+++ db/migrate/20230927175021_create_schedules.rb
@@ -1,6 +1,16 @@
 class CreateSchedules < ActiveRecord::Migration[7.0]
   def change
     create_table :schedules do |t|
+      t.date :start_date
+      t.date :end_date
+      t.integer :sunday_shift
+      t.integer :monday_shift
+      t.integer :tuesday_shift
+      t.integer :wednesday_shift
+      t.integer :thursday_shift
+      t.integer :friday_shift
+      t.integer :saturday_shift
+      t.integer :number_available
 
       t.timestamps
     end
