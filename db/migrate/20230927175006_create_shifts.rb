6d2803e39c9fed1a4fd5932f95234913365b242b
Andrew <andrewonulak@gmail.com>
2023-09-27 11:56:36 -0600

    creating table data for Shifts

--- db/migrate/20230927175006_create_shifts.rb
+++ db/migrate/20230927175006_create_shifts.rb
@@ -1,6 +1,10 @@
 class CreateShifts < ActiveRecord::Migration[7.0]
   def change
     create_table :shifts do |t|
+      t.string :position
+      t.boolean :day_off
+      t.integer :start_time
+      t.integer :off_time
 
       t.timestamps
     end
