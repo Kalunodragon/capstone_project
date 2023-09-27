c356f2eedf200347f3a9e136290a9ef1d7d0253c
Andrew <andrewonulak@gmail.com>
2023-09-27 12:17:49 -0600

    creating seed data for testing, without faker gem

---
+++ db/schema.rb
@@ -0,0 +1,60 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_175021) do
+  create_table "bids", force: :cascade do |t|
+    t.integer "choice_number"
+    t.integer "employee_id"
+    t.integer "schedule_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "department"
+    t.integer "phone_number"
+    t.string "email"
+    t.string "station"
+    t.date "seniority_date"
+    t.date "date_of_birth"
+    t.boolean "admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "sunday_shift"
+    t.integer "monday_shift"
+    t.integer "tuesday_shift"
+    t.integer "wednesday_shift"
+    t.integer "thursday_shift"
+    t.integer "friday_shift"
+    t.integer "saturday_shift"
+    t.integer "number_available"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shifts", force: :cascade do |t|
+    t.string "position"
+    t.boolean "day_off"
+    t.integer "start_time"
+    t.integer "off_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+p "--- Starting Seed Generator ---"
+p " "
+p "Generating Employees"
+    Employee.create(first_name:"Andrew", last_name:"Onulak", department:"Ramp", phone_number:7325759043, email:"test@test.com", station:"DEN", seniority_date:2016-05-19, date_of_birth:1992-05-02, admin:false)
+p "Finished generating Employees"
+p " "
+p "Generating Shifts"
+    Shift.create(position: "Off", day_off: true, start_time: nil, off_time: nil)
+    Shift.create(position: "Tpoint", day_off: false, start_time: 600, off_time: 1430)
+    Shift.create(position: "Gates", day_off: false, start_time: 1430, off_time: 2300)
+    Shift.create(position: "Transfers", day_off: false, start_time: 500, off_time: 1330)
+    Shift.create(position: "Locals", day_off: false, start_time: 1330, off_time: 2200)
+    Shift.create(position: "Lavs", day_off: false, start_time: 530, off_time: 1400)
+    Shift.create(position: "Cargo", day_off: false, start_time: 1400, off_time: 2230)
+    Shift.create(position: "Float", day_off: false, start_time: 900, off_time: 1730)
+p "Finished generating Shifts"
+p " "
+p "Generating Schedules"
+    Schedule.create(start_date: 2023-10-8, end_date: 2023-11-11, sunday_shift:1, monday_shift:1, tuesday_shift:2, wednesday_shift:2. thursday_shift:2, friday_shift:2, saturday_shift:2, number_available:3)
+    Schedule.create(start_date: 2023-10-8, end_date: 2023-11-11, sunday_shift:3, monday_shift:3, tuesday_shift:1, wednesday_shift:1. thursday_shift:3, friday_shift:3, saturday_shift:3, number_available:2)
+    Schedule.create(start_date: 2023-10-8, end_date: 2023-11-11, sunday_shift:4, monday_shift:4, tuesday_shift:4, wednesday_shift:4. thursday_shift:1, friday_shift:1, saturday_shift:4, number_available:1)
+    Schedule.create(start_date: 2023-10-8, end_date: 2023-11-11, sunday_shift:7, monday_shift:7, tuesday_shift:7, wednesday_shift:1. thursday_shift:1, friday_shift:7, saturday_shift:7, number_available:5)
+p "Finished generating Schedules"
+p " "
+p "Generating Bids"
+    Bid.create(choice_number:1, employee_id:1, schedule_id:1)
+    Bid.create(choice_number:2, employee_id:1, schedule_id:3)
+p "Finished generating Bids"
+p " "
+p "--- Finished Seed Generator ---"
