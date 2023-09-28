dd10004ea7094ad3a16dae90de9112cbbd2c7221
Andrew <andrewonulak@gmail.com>
2023-09-27 19:13:28 -0600

    leaving comment for where to pick back up on project for next session

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,8 +1,11 @@
 class EmployeesController < ApplicationController
     skip_before_action :auth, only: :create
 
+    # Finish setting up controller to allow for routes control of Employee model.
+    # Test with postman to make sure create, show, update, and destroy all work
+    
     def create
-        
+
     end
 
     def show
