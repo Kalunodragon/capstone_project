81b6a3577073fb60117d93819dc56081e556b58f
Andrew <andrewonulak@gmail.com>
2023-09-27 18:45:26 -0600

    setting up main controller for auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    include ActionController::Cookies
+    
+    rescue_from ActiveRecord::RecordInvalid, with: :render_not_processable
+    
+    private
+
+    def auth
+        @current_employee = Employee.find_by(id: session[:employee_id])
+        render json: { errors: "No authorization found, Please sign-in" }, status: :unauthorized unless @current_employee
+    end
+
+    def render_not_processable(item)
+        render json: { errors: item.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
