83e4d9f73a073baa7be7b52b9475d3359da60921
Andrew <andrewonulak@gmail.com>
2023-09-27 18:49:30 -0600

    working on setting up routes with auth

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,19 @@
 class EmployeesController < ApplicationController
+    skip_before_action :auth, only: :create
+
+    def create
+        
+    end
+
+    def show
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :schedules
   resources :shifts
   resources :bids
-  resources :employees
+  resources :employees, only: [:create, :show, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
