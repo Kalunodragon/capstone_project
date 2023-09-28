37b4c5d36ff7232bdb87d61c2cbe5d2866a7c087
Andrew <andrewonulak@gmail.com>
2023-09-27 19:10:06 -0600

    adding validations for all attributes on Employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,9 @@ class Employee < ApplicationRecord
     has_secure_password
 
     has_many :bids
-    has_many :schedules
-    has_many :shifts
+    has_many :schedules, through: :bids
+
+    validates :email, :phone_number, uniqueness: true
+    validates :first_name, :last_name, :department, :phone_number, :email, :station, :seniority_date, :date_of_birth, :password, presence: true
+    validates_inclusion_of :admin, in: [true, false]
 end
