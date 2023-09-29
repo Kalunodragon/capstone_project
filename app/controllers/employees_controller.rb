de8f1a1e64031812ab8241adcf36e8f865ba33b7
Andrew <andrewonulak@gmail.com>
2023-09-29 16:07:56 -0600

    Add the workings of creating a new Employee while also creating session information for the Employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -5,7 +5,13 @@ class EmployeesController < ApplicationController
     # Test with postman to make sure create, show, update, and destroy all work
     
     def create
-
+        if(params[:password] == params[password_confirmation])
+            employee = Employee.create!(employee_params)
+            session[:employee_id] = employee.id
+            render json: employee, status: :created
+        else
+            render json: { errors: "Please try again, can't process"}, status: :unprocessable_entity
+        end
     end
 
     def show
@@ -18,5 +24,11 @@ class EmployeesController < ApplicationController
 
     def destroy
 
+    end
+
+    private
+
+    def employee_params
+        params.permit(:first_name, :last_name, :department, :phone_number, :email, :station, :seniority_date, :date_of_birth, :admin, :password, :password_confirmation)
     end
 end
