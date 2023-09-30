ad32c05263e9378048bcf8f87abc6556be30a39f
Andrew <andrewonulak@gmail.com>
2023-09-29 20:03:00 -0600

    Removes phone_number uniqueness for testing and adds in a few more seed data to work with

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,7 +4,8 @@ class Employee < ApplicationRecord
     has_many :bids
     has_many :schedules, through: :bids
 
-    validates :email, :phone_number, uniqueness: true
+    # removed :phone_number from uniqueness: true
+    validates :email, uniqueness: true
     validates :first_name, :last_name, :department, :phone_number, :email, :station, :seniority_date, :date_of_birth, :password, presence: true
     validates_inclusion_of :admin, in: [true, false]
 end
