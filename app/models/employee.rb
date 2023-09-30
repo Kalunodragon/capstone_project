570952ec530a173849473aa1fa0f91beeeb798da
Andrew <andrewonulak@gmail.com>
2023-09-30 11:44:43 -0600

    Add self.seniority_list for sorting all employees in the database

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -8,4 +8,12 @@ class Employee < ApplicationRecord
     validates :email, uniqueness: true
     validates :first_name, :last_name, :department, :phone_number, :email, :station, :seniority_date, :date_of_birth, :password, presence: true
     validates_inclusion_of :admin, in: [true, false]
+
+    def self.seniority_list
+        sorted = Employee.all.sort_by { |a| [a.seniority_date, a.date_of_birth] }
+        sort_print = sorted.each do |per|
+            p per.first_name
+        end
+        sort_print
+    end
 end
