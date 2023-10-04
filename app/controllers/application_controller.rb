9e8f457c498e6bd61be568ba4410f03d7c80311f
Andrew <andrewonulak@gmail.com>
2023-10-04 16:10:55 -0600

    Second Attempt for preventing server shutdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,20 @@ class ApplicationController < ActionController::API
     rescue_from ActiveRecord::RecordInvalid, with: :render_not_processable
     before_action :auth
 
+    def prevent_server_shutdown
+        x = 1
+        t = (Time.now + 10*60).to_fs(:time)
+        p Time.now
+        while x > 0 do
+            if(Time.now >= t)
+                p "Server timer reset"
+                prevent_server_shutdown
+            end
+        end
+    end
+
+    prevent_server_shutdown
+
     private
 
     def auth
@@ -14,19 +28,5 @@ class ApplicationController < ActionController::API
     def render_not_processable(item)
         render json: { errors: item.record.errors.full_messages }, status: :unprocessable_entity
     end
-
-    def prevent_server_shutdown
-        x = 1
-        t = (Time.now + 10*60).to_fs(:time)
-        while x > 0 do
-            if(Time.now >= t)
-                p "Server timer reset"
-                p Time.now
-                prevent_server_shutdown
-            end
-        end
-    end
-
-    prevent_server_shutdown
 
 end
