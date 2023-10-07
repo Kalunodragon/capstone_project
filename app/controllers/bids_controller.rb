42b516c465106aacdf552802ed35179965e04e61
Andrew <andrewonulak@gmail.com>
2023-10-07 00:02:03 -0600

    Add both SMS methods from application controller

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -2,7 +2,7 @@ class BidsController < ApplicationController
 
   def award_bid
     if(@current_employee.admin)
-      if(Date.now.to_fs > Schedule.first.bid_close.to_fs)
+      if(Date.today.to_fs > Schedule.first.bid_close.to_fs)
         Bid.execute_bid
         Employee.seniority_list.each do |e|
           if(e.bids.all.find_by(awarded:true))
@@ -19,5 +19,40 @@ class BidsController < ApplicationController
       render json: { errors: "You are not authorized to award the bid." }, status: :unauthorized
     end
   end
+
+  private
+
+  def awarded_message(employee)
+    @client = Twilio::REST::Client.new ENV["ACCOUNT_SID"], ENV["AUTH_TOKEN"]
+
+    schedule_info = employee.bids.find_by(awarded:true).schedule
+    shift_days = schedule_info.shift_info
+    day_array = []
+    shift_days.each do |d|
+        if(d.day_off)
+            day_array << "Off"
+        else
+            day_array << "#{d.position}: #{d.start_time}-#{d.off_time}"
+        end
+    end
+
+    message_to_send = "#{employee.first_name}, Your schedule from #{schedule_info.start_date.to_fs(:long_ordinal)} to #{schedule_info.end_date.to_fs(:long_ordinal)} will be as follows: \nSunday: #{day_array[0]} \nMonday: #{day_array[1]} \nTuesday: #{day_array[2]} \nWednesday: #{day_array[3]} \nThursday: #{day_array[4]} \nFriday: #{day_array[5]} \nSaturday: #{day_array[6]}"
+
+    message = @client.messages.create(
+        body: message_to_send,
+        to: "+1" + employee.phone_number.to_s,
+        from: ENV["TWILIO_NUMBER"],
+    )
+end
+
+def not_enough_lines(employee)
+    @client = Twilio::REST::Client.new ENV["ACCOUNT_SID"], ENV["AUTH_TOKEN"]
+
+    message = @client.messages.create(
+        body: "#{employee.first_name}, You did not bid enough lines on the bid. Admin will contact you shortly about your schedule.",
+        to: "+1" + employee.phone_number.to_s,
+        from: ENV["TWILIO_NUMBER"],
+    )
+end
   
 end
