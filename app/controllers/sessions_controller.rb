c2ac94ff97b44452ef62dab632a89235e732b656
Andrew <andrewonulak@gmail.com>
2023-10-06 22:26:30 -0600

    Add sessions for login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  skip_before_action :auth, only: :create
+
+  def create
+    employee = Employee.find_by(first_name: params[:first_name], last_name: params[:last_name])
+    if employee&.authenticate(params[:password])
+      session[:employee_id] = employee.id
+      if(employee.admin)
+        session[:admin] = employee.admin
+      end
+      render json: employee, status: :ok
+    else
+      render json: { error: "Employee does not exist please try again." }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    session.delete :employee_id, :admin
+    head :no_content
+  end
+end
