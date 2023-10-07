390f95143326bf11e9f4b1245acf793ffbbcbb34
Andrew <andrewonulak@gmail.com>
2023-10-06 22:25:57 -0600

    Change layout of routes and add routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  get "/test", to: "employees#test"
+  get "/award_bid", to: "bids#award_bid"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
   resources :schedules
   resources :shifts
-  resources :bids
+  resources :bids, only: [:create, :show, :update, :destroy]
   resources :employees, only: [:create, :show, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  get "/test", to: "employees#test"
 end
