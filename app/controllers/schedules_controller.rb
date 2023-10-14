a042e05163bae94f4a7db7e110088fd21accd94d
Andrew <andrewonulak@gmail.com>
2023-10-14 00:06:40 -0600

    Add methods for routes

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,26 @@
 class SchedulesController < ApplicationController
+
+  def create
+    
+  end
+
+  def index
+    if(@current_employee)
+      render json: Schedule.all, status: :ok
+    else
+      render json: { errors: "Please login to view this content!" }, status: :unauthorized
+    end
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
