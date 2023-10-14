adfe09d0ab6af2e64ded2e9688e72b3c3a4960d1
Andrew <andrewonulak@gmail.com>
2023-10-14 02:43:15 -0600

    Create and fill out schedule controller

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,26 +1,54 @@
 class SchedulesController < ApplicationController
 
   def create
-    
+    if(@current_employee.admin)
+      created_schedule = Schedule.find_or_create_by!(schedule_params)
+      render json: created_schedule, serializer: ScheduleSerializer, status: :created
+    else
+      render json: { errors: "Please login to preform this action!" }, status: :unauthorized
+    end
   end
 
   def index
     if(@current_employee)
-      render json: Schedule.all, status: :ok
+      render json: Schedule.all, each_serializer: ScheduleSerializer, status: :ok
     else
       render json: { errors: "Please login to view this content!" }, status: :unauthorized
     end
   end
 
   def show
-
+    if(@current_employee)
+      schedule_to_show = Schedule.find_by(id: params[:id])
+      render json: schedule_to_show, serializer: ScheduleSerializer, status: :ok
+    else
+      render json: { errors: "Please login to view this content!" }, status: :unauthorized
+    end
   end
 
   def update
-
+    if(@current_employee.admin)
+      schedule_to_update = Schedule.find_by(id: params[:id])
+      schedule_to_update.update!(schedule_params)
+      render json: schedule_to_update, serializer: ScheduleSerializer, status: :accepted
+    else
+      render json: { errors: "Please login to view this content!" }, status: :unauthorized
+    end
   end
 
   def destroy
+    if(@current_employee.admin)
+      schedule_to_delete = Schedule.find_by(id: params[:id])
+      schedule_to_delete.destroy
+      render json: schedule_to_delete, serializer: ScheduleSerializer, status: :ok
+    else
+      render json: { errors: "Please login to view this content!" }, status: :unauthorized
+    end
+  end
 
+  private
+
+  def schedule_params
+    params.permit(:bid_open,:bid_close,:start_date,:end_date,:sunday_shift,:monday_shift,:tuesday_shift,:wednesday_shift,:thursday_shift,:friday_shift,:saturday_shift,:number_available)
   end
 end
