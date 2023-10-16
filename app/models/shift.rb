a7e56f8d9a443b01b5d6d76e44fa5e1590878322
Andrew <andrewonulak@gmail.com>
2023-10-16 12:22:09 -0600

    Comment left for regex for possible time validation

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -5,4 +5,5 @@ class Shift < ApplicationRecord
     validates :start_time, :off_time, presence: true, allow_blank: true
     validates_inclusion_of :day_off, in: [true, false]
 
+    # format: { with: /\A([0-1][0-9]|2[0-3]):([0-5][0-9])\z/, message:  "Please enter time in military time format" }
 end
