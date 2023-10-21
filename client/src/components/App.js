8c672bfe00b2e855b582149b5d9f7879a3e2a9b2
Andrew <andrewonulak@gmail.com>
2023-10-21 15:25:14 -0600

    Testing out mui

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { Typography, AppBar, Toolbar, IconButton } from '@mui/material'
+import MenuIcon from "@mui/icons-material/Menu"
 
 function App(){
 
   return(
-    <h1>Hello World</h1>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Typography variant="h1" align="center">RADBP</Typography>
+      <Typography variant="h5" align="center">
+        Ramp Agent Digital Bidding Platform
+      </Typography>
+    </>
   )
 }
 
