ea3902dcaa6f7f23bb449972501866a425487607
Andrew <andrewonulak@gmail.com>
2023-10-21 20:15:17 -0600

    Fix route for GET employee

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
   get "/award_bid", to: "bids#award_bid"
   get "/test", to: "employees#test"
+  get "/employee", to: "employees#show"
 
   resources :schedules, only: [:create, :index, :show, :update, :destroy]
   resources :shifts, only: [:create, :index, :show, :update, :destroy]
   resources :bids, only: [:create, :show, :index, :update, :destroy]
-  resources :employees, only: [:create, :show, :update, :destroy]
+  resources :employees, only: [:create, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
