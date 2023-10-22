a73f6e8a37384a9e500ccc933074af5aa5f550f7
Andrew <andrewonulak@gmail.com>
2023-10-22 16:36:14 -0600

    Fix time formatting for Twilio messages

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,4 +1,5 @@
 class BidsController < ApplicationController
+  skip_before_action :auth, only: :test
 
   def create
     if(@current_employee)
@@ -99,11 +100,11 @@ class BidsController < ApplicationController
         if(d.day_off)
             day_array << "Off"
         else
-            day_array << "#{d.position}: #{d.start_time}-#{d.off_time}"
+            day_array << "#{d.position}: #{d.start_time.strftime("%R")}-#{d.off_time.strftime("%R")}"
         end
     end
 
-    message_to_send = "#{employee.first_name}, Your schedule from #{schedule_info.start_date.to_fs(:long_ordinal)} to #{schedule_info.end_date.to_fs(:long_ordinal)} will be as follows: \nSunday: #{day_array[0]} \nMonday: #{day_array[1]} \nTuesday: #{day_array[2]} \nWednesday: #{day_array[3]} \nThursday: #{day_array[4]} \nFriday: #{day_array[5]} \nSaturday: #{day_array[6]}"
+    message_to_send = "#{employee.first_name}, Your schedule from #{schedule_info.start_date.to_fs(:long_ordinal)} to #{schedule_info.end_date.to_fs(:long_ordinal)} will be as follows: \n\nSunday: #{day_array[0]} \nMonday: #{day_array[1]} \nTuesday: #{day_array[2]} \nWednesday: #{day_array[3]} \nThursday: #{day_array[4]} \nFriday: #{day_array[5]} \nSaturday: #{day_array[6]}"
 
     message = @client.messages.create(
         body: message_to_send,
