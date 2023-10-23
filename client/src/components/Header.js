8a5857d7f905208837e40e395bb3c4d11f3f9a86
Andrew <andrewonulak@gmail.com>
2023-10-23 15:47:15 -0600

    Test logout from button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Typography, AppBar, Toolbar, IconButton, Button } from '@mui/material'
 import MenuIcon from "@mui/icons-material/Menu"
 import AccountCircle from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
 import { employeeContext } from "./App";
 
 function Header({ onLogout }){
@@ -18,17 +19,25 @@ function Header({ onLogout }){
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h4" align="center" sx={{ flexGrow: 1 }}>{employee.admin ? "ADMIN-RADS" : "RADD"}</Typography>
+          <Typography variant="h4" align="center" sx={{ flexGrow: 1 }}>{employee.admin ? "ADMIN-RADS" : "RADS"}</Typography>
           <IconButton
             size="large"
             color="inherit"
             aria-label="menu"
           >
+            <LogoutIcon onClick={()=>fetch("/logout",{method:"DELETE"}).then(onLogout(null))}/>
+          </IconButton>
+          <IconButton
+            size="large"
+            color="inherit"
+            aria-label="menu"
+          >
+            {/* Move this to a sidebar type thing */}
             <AccountCircle />
           </IconButton>
         </Toolbar>
       </AppBar>
-      <Button variant="contained" align="center" onClick={()=>fetch("/logout",{method:"DELETE"}).then(onLogout(null))}>Logout</Button>
+      {/* <Button variant="contained" align="center">Logout</Button> */}
     </>
   )
 }
