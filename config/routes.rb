65eae662c7d14638ed62ee68d0cdd991cb9b3c27
Andrew <andrewonulak@gmail.com>
2023-10-25 14:43:10 -0600

    Add route for fallback controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   resources :bids, only: [:create, :show, :index, :update, :destroy]
   resources :employees, only: [:create, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '*path',
+    to: 'fallback#index',
+    constraints: ->(req) { !req.xhr? && req.format.html? }
 
   # Defines the root path route ("/")
   # root "articles#index"
