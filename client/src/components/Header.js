fd8839e4cbf3e9704b03df90bc28cf990cb6af48
Andrew <andrewonulak@gmail.com>
2023-10-24 23:34:21 -0600

    Create navigation bar as well as profile bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { Typography, AppBar, Toolbar, IconButton, Button } from '@mui/material'
+import React, { useContext, useState } from "react";
+import { Typography, AppBar, Toolbar, IconButton, Drawer, Box, Divider } from '@mui/material'
 import MenuIcon from "@mui/icons-material/Menu"
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import LogoutIcon from '@mui/icons-material/Logout';
@@ -7,37 +7,67 @@ import { employeeContext } from "./App";
 
 function Header({ onLogout }){
   const employee = useContext(employeeContext)
+  const [leftDrawerOpen, setLeftDrawerOpen] = useState(false)
+  const [rightDrawerOpen, setRightDrawerOpen] = useState(false)
+
 
   return(
     <>
       <AppBar position="static">
         <Toolbar>
           <IconButton
+            title="Navigation"
             size="large"
             color="inherit"
-            aria-label="menu"
+            onClick={()=>setLeftDrawerOpen(true)}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h4" align="center" sx={{ flexGrow: 1 }}>{employee.admin ? "ADMIN-RADS" : "RADS"}</Typography>
           <IconButton
+            title="Account"
             size="large"
             color="inherit"
-            aria-label="menu"
+            onClick={()=>setRightDrawerOpen(true)}
           >
-            <LogoutIcon onClick={()=>fetch("/logout",{method:"DELETE"}).then(onLogout(null))}/>
-          </IconButton>
-          <IconButton
-            size="large"
-            color="inherit"
-            aria-label="menu"
-          >
-            {/* Move this to a sidebar type thing */}
             <AccountCircle />
           </IconButton>
         </Toolbar>
       </AppBar>
-      {/* <Button variant="contained" align="center">Logout</Button> */}
+      <Drawer
+        anchor="left"
+        open={leftDrawerOpen}
+        onClose={()=>setLeftDrawerOpen(false)}
+      >
+        <Box p={2} textAlign="center" width={200}>
+          <Typography>
+            Navigation
+          </Typography>
+        </Box>
+      </Drawer>
+      <Drawer
+        anchor="right"
+        open={rightDrawerOpen}
+        onClose={()=>setRightDrawerOpen(false)}
+      >
+        <Box p={2} textAlign="center" width={200}>
+          <Typography>
+            Profile
+          </Typography>
+          <Divider />
+          <IconButton
+            onClick={()=>fetch("/logout",{method:"DELETE"}).then(onLogout(null))}
+            size="large"
+            color="inherit"
+            sx={{ borderRadius: 0 }}
+          >
+            <Typography>
+              Logout
+            </Typography>
+            <LogoutIcon />
+          </IconButton>
+        </Box>
+      </Drawer>
     </>
   )
 }
