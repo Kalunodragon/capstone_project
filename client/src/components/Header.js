c68bb1b19f21a2e7544420b16bfb34013fd34ac1
Andrew <andrewonulak@gmail.com>
2023-10-26 00:02:44 -0600

    Create seperate left navigation for employee and admin

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,34 +13,9 @@ function Header({ onLogout }){
   const [leftDrawerOpen, setLeftDrawerOpen] = useState(false)
   const [rightDrawerOpen, setRightDrawerOpen] = useState(false)
 
-  // Create if(employee.admin) return(Admin navigation bar)
-  // Find out what an onClick(e.target / e.currentTarget) console.log()'s
-  // Set up all component pages, for both employee and admin side
-  // Set up handleClose() to close sidebars L & R
-
-  return(
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            title="Navigation"
-            size="large"
-            color="inherit"
-            onClick={()=>setLeftDrawerOpen(true)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h4" align="center" sx={{ flexGrow: 1 }}>{employee.admin ? "RADS-ADMIN" : "RADS"}</Typography>
-          <IconButton
-            title="Account"
-            size="large"
-            color="inherit"
-            onClick={()=>setRightDrawerOpen(true)}
-          >
-            <AccountCircle />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
+  let leftDrawer
+    if(!employee.admin){
+      leftDrawer = <>
       <Drawer
         anchor="left"
         open={leftDrawerOpen}
@@ -111,6 +86,123 @@ function Header({ onLogout }){
           </IconButton>
         </Box>
       </Drawer>
+      </>
+    }
+    if(employee.admin){
+      leftDrawer = <>
+      <Drawer
+        anchor="left"
+        open={leftDrawerOpen}
+        onClose={()=>setLeftDrawerOpen(false)}
+      >
+        <Box p={2} textAlign="center" width={200}>
+          <IconButton
+            size="large"
+            color="inherit"
+            sx={{ borderRadius: 0 }}
+          >
+            <FormatListNumberedIcon />
+            <Typography>
+              Schedules
+            </Typography>
+          </IconButton>
+          <Divider />
+          <IconButton
+            size="large"
+            color="inherit"
+            sx={{ borderRadius: 0 }}
+          >
+            <CalendarMonthIcon />
+            <Typography>
+              Shifts
+            </Typography>
+          </IconButton>
+          <Divider />
+          <IconButton
+            size="large"
+            color="inherit"
+            sx={{ borderRadius: 0 }}
+          >
+            <InfoIcon />
+            <Typography>
+              Employees
+            </Typography>
+          </IconButton>
+          <Divider />
+          <IconButton
+            size="large"
+            color="inherit"
+            sx={{ borderRadius: 0 }}
+          >
+            <InfoIcon />
+            <Typography>
+              App Info
+            </Typography>
+          </IconButton>
+        </Box>
+      </Drawer>
+      <Drawer
+        anchor="right"
+        open={rightDrawerOpen}
+        onClose={()=>setRightDrawerOpen(false)}
+      >
+        <Box p={2} textAlign="center" width={200}>
+          <IconButton
+            size="large"
+            color="inherit"
+            sx={{ borderRadius: 0 }}
+          >
+            <AccountCircle />
+            <Typography>
+              Account
+            </Typography>
+          </IconButton>
+          <Divider />
+          <IconButton
+            onClick={()=>fetch("/logout",{method:"DELETE"}).then(onLogout(null))}
+            size="large"
+            color="inherit"
+            sx={{ borderRadius: 0 }}
+          >
+            <LogoutIcon />
+            <Typography>
+              Logout
+            </Typography>
+          </IconButton>
+        </Box>
+      </Drawer>
+      </>
+    }
+
+  // Create if(employee.admin) return(Admin navigation bar)
+  // Find out what an onClick(e.target / e.currentTarget) console.log()'s
+  // Set up all component pages, for both employee and admin side
+  // Set up handleClose() to close sidebars L & R
+
+  return(
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            title="Navigation"
+            size="large"
+            color="inherit"
+            onClick={()=>setLeftDrawerOpen(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h4" align="center" sx={{ flexGrow: 1 }}>{employee.admin ? "RADS-ADMIN" : "RADS"}</Typography>
+          <IconButton
+            title="Account"
+            size="large"
+            color="inherit"
+            onClick={()=>setRightDrawerOpen(true)}
+          >
+            <AccountCircle />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      {leftDrawer}
     </>
   )
 }
