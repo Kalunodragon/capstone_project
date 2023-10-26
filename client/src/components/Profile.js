09e3d5d96469a4a296549b999b2f6c97160095d7
Andrew <andrewonulak@gmail.com>
2023-10-26 01:41:03 -0600

    Add more information to profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -15,7 +15,24 @@ function Profile(){
           </Typography>
           <Divider />
           <Typography variant="subtitle1" align="left">
-            {employee.department}
+            {employee.station} - {employee.department}
+          </Typography>
+        </Paper>
+        <Paper className="profile">
+          <Typography variant="h5" align="left">
+            Main Info:
+          </Typography>
+          <Divider />
+          <Typography variant="subtitle2" align="left">
+            Phone number: {employee.phone_number}
+          </Typography>
+          <Divider />
+          <Typography variant="subtitle2" align="left">
+            Email: {employee.email}
+          </Typography>
+          <Divider />
+          <Typography variant="subtitle2" align="left">
+            Hire Date: {employee.seniority_date}
           </Typography>
         </Paper>
       </Container> 
