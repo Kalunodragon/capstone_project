1e372fa4ff990c8feb27cd63c766ff1330d39a3b
Andrew <andrewonulak@gmail.com>
2023-10-28 15:42:48 -0600

    Left comment for how to set up alert on home screen

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -7,7 +7,12 @@ function MainPage(){
   const navigate = useNavigate()
   const employee = useContext(employeeContext)
 
-  const bidOpen = true
+  // Create Backend Route for just checking if a bid is open
+    // This should run in a useEffect() and set bidOpen to true or false
+    // if bid is open have it return the date range of bidding period
+    // it should also check if the @currentEmployee has placed a bid for said bid
+
+  const bidOpen = false
   const bidSubmitted = false
 
   if(!employee){
@@ -21,8 +26,12 @@ function MainPage(){
               Hello {employee.first_name}!
             </Typography>
             {bidOpen ? (bidSubmitted ? 
-              <><Alert variant="filled" severity="success">Bid Submitted</Alert><br/></> :
-              <><Button align="center" variant="filled" severity="error">Create Bid</Button><br/></>) :
+              <><Alert variant="filled" severity="success">
+                Bid Submitted
+                </Alert><br/></> :
+              <><Alert align="center" variant="filled" severity="warning" style={{ "backgroundColor":"#f9b612"}}>
+                Bid is now open!
+              </Alert><br/></>) :
               null
             }
             <Divider />
@@ -33,9 +42,8 @@ function MainPage(){
             <Divider />
             <Typography variant="p">
               Quick reference: Top Left is the Navigation tab, Top Right is the Profile tab. If a bid is open you will 
-              see a button above this area with "Create Bid", after a bid is submitted this button will change to a 
-              "Bid Submitted" notification. To edit your account head to your Account and then Settings in the Top 
-              Right from your Account.
+              see an alert above this area, after a bid is submitted this alert will change to a "Bid Submitted" 
+              notification. To edit your account head to your Account and then Settings in the Top Right from your Account.
             </Typography>
             <Divider />
             <Typography variant="p">
