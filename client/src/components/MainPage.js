4bd09ab39a0e87082a2c89dd1dfbbfecb0b8aea6
Andrew <andrewonulak@gmail.com>
2023-10-28 15:01:53 -0600

    create initial main/home page for Employee

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Alert, Button, Container, Divider, Paper, Typography } from "@mui/material";
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { employeeContext } from "./App";
@@ -7,13 +7,43 @@ function MainPage(){
   const navigate = useNavigate()
   const employee = useContext(employeeContext)
 
+  const bidOpen = true
+  const bidSubmitted = false
+
   if(!employee){
     navigate("/sign-in")
   } else {
     return(
-      <Typography>
-        Employee main page
-      </Typography>
+      <>
+        <Container align="center" className="profile">
+          <Paper className="profile">
+            <Typography variant="h4" align="center">
+              Hello {employee.first_name}!
+            </Typography>
+            {bidOpen ? (bidSubmitted ? 
+              <><Alert variant="filled" severity="success">Bid Submitted</Alert><br/></> :
+              <><Button align="center" variant="filled" severity="error">Create Bid</Button><br/></>) :
+              null
+            }
+            <Divider />
+            <Typography variant="p">
+              Welcome to the Ramp Agent Digital Scheduling system, RADS for short! This System is designed to make the
+              whole process of schedule bidding as easy as possible for an Agent.
+            </Typography>
+            <Divider />
+            <Typography variant="p">
+              Quick reference: Top Left is the Navigation tab, Top Right is the Profile tab. If a bid is open you will 
+              see a button above this area with "Create Bid", after a bid is submitted this button will change to a 
+              "Bid Submitted" notification. To edit your account head to your Account and then Settings in the Top 
+              Right from your Account.
+            </Typography>
+            <Divider />
+            <Typography variant="p">
+              Happy Bidding!
+            </Typography>
+          </Paper>
+        </Container>
+      </>
     )
   }
 }
