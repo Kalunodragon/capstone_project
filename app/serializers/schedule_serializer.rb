b75be515a052e473c35837e93c17e862bba9fc74
Andrew <andrewonulak@gmail.com>
2023-11-08 14:01:29 -0700

    Create serialized data inside of the schedule serializer for shifts

--- app/serializers/schedule_serializer.rb
+++ app/serializers/schedule_serializer.rb
@@ -3,15 +3,28 @@ class ScheduleSerializer < ActiveModel::Serializer
 
   def shifts
     days = []
-    days << {day:"Sunday", shift:Shift.find_by(id: object.sunday_shift)}
-    days << {day:"Monday", shift:Shift.find_by(id: object.monday_shift)}
-    days << {day:"Tuesday", shift:Shift.find_by(id: object.tuesday_shift)}
-    days << {day:"Wednesday", shift:Shift.find_by(id: object.wednesday_shift)}
-    days << {day:"Thursday", shift:Shift.find_by(id: object.thursday_shift)}
-    days << {day:"Friday", shift:Shift.find_by(id: object.friday_shift)}
-    days << {day:"Saturday", shift:Shift.find_by(id: object.saturday_shift)}
+    days << {day:"Sunday", shift: formatted_shift(object.sunday_shift)}
+    days << {day:"Tuesday", shift: formatted_shift(object.tuesday_shift)}
+    days << {day:"Wednesday", shift: formatted_shift(object.wednesday_shift)}
+    days << {day:"Thursday", shift: formatted_shift(object.thursday_shift)}
+    days << {day:"Friday", shift: formatted_shift(object.friday_shift)}
+    days << {day:"Saturday", shift: formatted_shift(object.saturday_shift)}
 
     days
+  end
+
+  def formatted_shift(shift_info)
+    shift = Shift.find_by(id: shift_info)
+    time_off = (shift.day_off ? shift.off_time : Time.at(shift.off_time).strftime("%R"))
+    time_start = (shift.day_off ? shift.start_time : Time.at(shift.start_time).strftime("%R"))
+    formatted = {
+      "id": shift.id,
+      "day_off": shift.day_off,
+      "off_time": time_off,
+      "start_time": time_start,
+      "position": shift.position
+    }
+    formatted
   end
 
 end
