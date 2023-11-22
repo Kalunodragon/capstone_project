5f40736ae9e9319f25fb116ceba8d3191e9512fc
Andrew <andrewonulak@gmail.com>
2023-11-21 20:36:07 -0700

    Create useEffect for fetching bid information

--- client/src/components/EmployeePastSchedule.js
+++ client/src/components/EmployeePastSchedule.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Loading from "./Loading"
 
-function EmployeePastSchedule(){
+function EmployeePastSchedule({ scheduleArray }){
+  const [loading, setLoading] = useState(false)
+
+  useEffect(()=>{
+    fetch("/bids")
+    .then((res)=>{
+      if(res.ok){
+        res.json()
+        .then((d)=>{
+
+          console.log(d)
+          setLoading(true)
+        })
+      } else {
+        res.json()
+        .then((d)=>{
+
+          setLoading(true)
+        })
+      }
+    })
+  },[])
+
+  if(!loading){
+    return(
+      <Loading />
+    )
+  }
 
   return(
     <h1>PAST COMP TEST</h1>
