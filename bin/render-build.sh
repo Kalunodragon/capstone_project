f5f48a6adfeec85cea307d1d4a20eaf5e7226236
Andrew <andrewonulak@gmail.com>
2023-11-29 10:10:58 -0700

    Reset render database

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -11,6 +11,6 @@ cp -a client/build/. public/
 bundle install
 # bundle exec rake db:create
 bundle exec rake db:migrate
-bundle exec rake db:reset
+bundle exec rake db:migrate:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rake db:seed
 # bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
